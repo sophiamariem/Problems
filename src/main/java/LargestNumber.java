04b76f5d269eaafd925a05f4b612af0f3b80087f
Sophia <sophiamariem@hotmail.com>
2020-03-02 23:37:52 +0000

    added faster largest num solution

--- src/main/java/LargestNumber.java
+++ src/main/java/LargestNumber.java
@@ -1,4 +1,7 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class LargestNumber {
 
@@ -6,5 +9,30 @@ public class LargestNumber {
         String[] stringArray = Arrays.stream(nums).mapToObj(String::valueOf).toArray(String[]::new);
         Arrays.sort(stringArray, (a, b) -> (b + a).compareTo(a + b));
         return String.join("", stringArray).replaceFirst("^0+(?!$)", "");
+    }
+
+    public String largestNumberFaster(int[] nums) {
+        List<String> numsList = new ArrayList<>(nums.length);
+        for (int i = 0; i < nums.length; i++) {
+            numsList.add(Integer.toString(nums[i]));
+        }
+
+        Collections.sort(numsList, (a, b)-> {
+            String s1 = a + b;
+            String s2 = b + a;
+            return s2.compareTo(s1);
+        });
+
+        int k = 0;
+        while (k < numsList.size() - 1 && numsList.get(k).equals("0")) {
+            k++;
+        }
+
+        StringBuffer result = new StringBuffer();
+        for (int i = k; i < numsList.size(); i++) {
+            result.append(numsList.get(i));
+        }
+
+        return result.toString();
     }
 }
--- src/test/java/LargestNumberTest.java
+++ src/test/java/LargestNumberTest.java
@@ -9,15 +9,18 @@ public class LargestNumberTest {
     @Test
     public void testOne() {
         assertEquals("210", largestNumber.largestNumber(new int[]{10, 2}));
+        assertEquals("210", largestNumber.largestNumberFaster(new int[]{10, 2}));
     }
 
     @Test
     public void testTwo() {
         assertEquals("9534330", largestNumber.largestNumber(new int[]{3, 30, 34, 5, 9}));
+        assertEquals("9534330", largestNumber.largestNumberFaster(new int[]{3, 30, 34, 5, 9}));
     }
 
     @Test
     public void testThree() {
         assertEquals("650", largestNumber.largestNumber(new int[]{05, 006, 0}));
+        assertEquals("650", largestNumber.largestNumberFaster(new int[]{05, 006, 0}));
     }
 }
