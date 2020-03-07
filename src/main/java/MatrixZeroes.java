2a0f0f50eba6a5e76b754739b6f333785163a281
Sophia <sophiamariem@hotmail.com>
2020-03-07 15:29:13 +0000

    matrix zeroes

---
+++ src/main/java/MatrixZeroes.java
@@ -0,0 +1,84 @@
+public class MatrixZeroes {
+
+    public int[][] setZeroesDfs(int[][] matrix) {
+        int cols = matrix.length;
+        int rows = matrix[0].length;
+        boolean[][] visited = new boolean[cols][rows];
+
+        for (int i = 0; i < cols; i++) {
+            for (int j = 0; j < rows; j++) {
+                if (matrix[i][j] == 0 && !visited[i][j]) {
+                    dfs(matrix, visited, i, j, cols, rows);
+                }
+            }
+        }
+        return matrix;
+    }
+
+    private void dfs(int[][] matrix, boolean[][] visited, int col, int row, int cols, int rows) {
+        visited[col][row] = true;
+
+        for (int i = 0; i < cols; i++) {
+            if (matrix[i][row] != 0) {
+                visited[i][row] = true;
+            }
+            matrix[i][row] = 0;
+        }
+
+        for (int j = 0; j < rows; j++) {
+            if (matrix[col][j] != 0) {
+                visited[col][j] = true;
+            }
+            matrix[col][j] = 0;
+        }
+    }
+
+    public int[][] setZeroes(int[][] matrix) {
+        boolean firstRowHasZero = false;
+        boolean firstColumnHasZero = false;
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    if (i == 0) firstColumnHasZero = true;
+                    if (j == 0) firstRowHasZero = true;
+
+                    if (i + j != 0) {
+                        matrix[i][0] = 0;
+                        matrix[0][j] = 0;
+                    }
+                }
+            }
+        }
+
+        for (int i = 1; i < matrix.length; i++) {
+            if (matrix[i][0] == 0) {
+                for (int j = 0; j < matrix[0].length; j++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        for (int j = 1; j < matrix[0].length; j++) {
+            if (matrix[0][j] == 0) {
+                for (int i = 0; i < matrix.length; i++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        if (firstColumnHasZero == true) {
+            for (int i = 0; i < matrix.length; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+
+        if (firstRowHasZero == true) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        return matrix;
+    }
+}
---
+++ src/test/java/MatrixZeroesTest.java
@@ -0,0 +1,56 @@
+import static org.junit.Assert.assertArrayEquals;
+
+import org.junit.Test;
+
+public class MatrixZeroesTest {
+
+    private MatrixZeroes matrixZeroes = new MatrixZeroes();
+
+    @Test
+    public void testForTwoRowsDfs() {
+        int[][] input = new int[][]{{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+        int[][] expected = new int[][]{{1, 0, 1}, {0, 0, 0}, {1, 0, 1}};
+
+        assertArrayEquals(expected, matrixZeroes.setZeroesDfs(input));
+    }
+
+    @Test
+    public void testForThreeRowsDfs() {
+        int[][] input = new int[][]{{0, 1, 2, 0}, {3, 4, 5, 2}, {1, 3, 1, 5}};
+        int[][] expected = new int[][]{{0, 0, 0, 0}, {0, 4, 5, 0}, {0, 3, 1, 0}};
+
+        assertArrayEquals(expected, matrixZeroes.setZeroesDfs(input));
+    }
+
+    @Test
+    public void testForZeroesFirstRowDfs() {
+        int[][] input = new int[][]{{0, 0, 0}, {1, 0, 1}, {1, 1, 1}};
+        int[][] expected = new int[][]{{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+
+        assertArrayEquals(expected, matrixZeroes.setZeroesDfs(input));
+    }
+
+    @Test
+    public void testForTwoRows() {
+        int[][] input = new int[][]{{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+        int[][] expected = new int[][]{{1, 0, 1}, {0, 0, 0}, {1, 0, 1}};
+
+        assertArrayEquals(expected, matrixZeroes.setZeroes(input));
+    }
+
+    @Test
+    public void testForThreeRows() {
+        int[][] input = new int[][]{{0, 1, 2, 0}, {3, 4, 5, 2}, {1, 3, 1, 5}};
+        int[][] expected = new int[][]{{0, 0, 0, 0}, {0, 4, 5, 0}, {0, 3, 1, 0}};
+
+        assertArrayEquals(expected, matrixZeroes.setZeroes(input));
+    }
+
+    @Test
+    public void testForZeroesFirstRow() {
+        int[][] input = new int[][]{{0, 0, 0}, {1, 0, 1}, {1, 1, 1}};
+        int[][] expected = new int[][]{{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+
+        assertArrayEquals(expected, matrixZeroes.setZeroes(input));
+    }
+}
