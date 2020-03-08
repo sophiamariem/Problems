cc2b8d7148c39c0487a38bde2e918f0da60a73c5
Sophia <sophiamariem@hotmail.com>
2020-03-08 16:16:26 +0000

    MergeTwoSortedLists

--- src/main/java/MergeTwoSortedLists.java
+++ src/main/java/MergeTwoSortedLists.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
-
 public class MergeTwoSortedLists {
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+
+        if (l2 == null) {
+            return l1;
+        }
+
+        if (l1.val > l2.val) {
+            return mergeTwoLists(l2, l1);
+        }
+
+        l1.next = mergeTwoLists(l1.next, l2);
+        return l1;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+
+        public void next(ListNode next) {
+            this.next = next;
+        }
+    }
 }
--- src/test/java/MergeTwoSortedListsTest.java
+++ src/test/java/MergeTwoSortedListsTest.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
 
 public class MergeTwoSortedListsTest {
+    private MergeTwoSortedLists mergeTwoSortedLists = new MergeTwoSortedLists();
+
+    @Test
+    public void test() {
+        MergeTwoSortedLists.ListNode l1 = new MergeTwoSortedLists.ListNode(1);
+        MergeTwoSortedLists.ListNode l12 =new MergeTwoSortedLists.ListNode(2);
+        MergeTwoSortedLists.ListNode l13 =new MergeTwoSortedLists.ListNode(4);
+        l1.next(l12);
+        l12.next(l13);
+
+        MergeTwoSortedLists.ListNode l2 = new MergeTwoSortedLists.ListNode(1);
+        MergeTwoSortedLists.ListNode l22 =new MergeTwoSortedLists.ListNode(3);
+        MergeTwoSortedLists.ListNode l23 =new MergeTwoSortedLists.ListNode(4);
+        l2.next(l22);
+        l22.next(l23);
+
+        MergeTwoSortedLists.ListNode result = mergeTwoSortedLists.mergeTwoLists(l1, l2);
+        assertEquals(1, result.val);
+        assertEquals(1, result.next.val);
+        assertEquals(2, result.next.next.val);
+        assertEquals(3, result.next.next.next.val);
+        assertEquals(4, result.next.next.next.next.val);
+        assertEquals(4, result.next.next.next.next.next.val);
+    }
 }
