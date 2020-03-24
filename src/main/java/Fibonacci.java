55d5887dd9b9fcc39472db7a65caaf29c7c2f72d
sophia <sophiamariem@hotmail.com>
2020-03-24 17:59:01 +0000

    Fibonacci

--- src/main/java/Fibonacci.java
+++ src/main/java/Fibonacci.java
@@ -15,4 +15,38 @@ public class Fibonacci {
         return memo[n];
     }
 
+    public int fibonacciBottomUpMemo(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+
+        int[] memo = new int[n];
+        memo[0] = 0;
+        memo[1] = 1;
+
+        for (int i = 2; i < n; i++) {
+            memo[i] = memo[i - 1] + memo[i - 2];
+        }
+
+        return memo[n - 1] + memo[n - 2];
+    }
+
+    public int fibonacciBottomUp(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+
+        int a = 0;
+        int b = 1;
+
+
+        for (int i = 2; i < n; i++) {
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+
+        return a + b;
+    }
+
 }
--- src/test/java/FibonacciTest.java
+++ src/test/java/FibonacciTest.java
@@ -7,9 +7,23 @@ public class FibonacciTest {
     private Fibonacci fibonacci = new Fibonacci();
 
     @Test
-    public void test() {
+    public void testMemo() {
         assertEquals(1, fibonacci.fib(2));
         assertEquals(2, fibonacci.fib(3));
         assertEquals(3, fibonacci.fib(4));
+    }
+
+    @Test
+    public void testBottomUpMemo() {
+        assertEquals(1, fibonacci.fibonacciBottomUpMemo(2));
+        assertEquals(2, fibonacci.fibonacciBottomUpMemo(3));
+        assertEquals(3, fibonacci.fibonacciBottomUpMemo(4));
+    }
+
+    @Test
+    public void testBottomUp() {
+        assertEquals(1, fibonacci.fibonacciBottomUp(2));
+        assertEquals(2, fibonacci.fibonacciBottomUp(3));
+        assertEquals(3, fibonacci.fibonacciBottomUp(4));
     }
 }
