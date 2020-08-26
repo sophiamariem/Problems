af6dafa8e8a5cea54f464dfd8a365f67516bf04b
Sophia <sophiamariem@hotmail.com>
2020-08-26 21:00:59 +0100

    Fizz Buzz

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,10 +1,13 @@
 //Write a program that prints the numbers from 1 to 100 and for multiples of '3' print "Fizz"
 //instead of the number and for the multiples of '5' print "Buzz".
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class FizzBuzz {
 
     public void fizzBuzz() {
-        for (int i=1; i <=100; i++) {
+        for (int i = 1; i <= 100; i++) {
             if (i % 3 == 0 & i % 5 == 0) {
                 System.out.println("Fizz Buzz");
             } else if (i % 3 == 0) {
@@ -15,5 +18,24 @@ public class FizzBuzz {
                 System.out.println(i);
             }
         }
+    }
+
+    // 1 to N
+    public List<String> fizzBuzz(int n) {
+        List<String> list = new LinkedList<>();
+        for (int i = 1; i <= n; i++) {
+            StringBuilder sb = new StringBuilder();
+            if (i % 3 == 0) {
+                sb.append("Fizz");
+            }
+            if (i % 5 == 0) {
+                sb.append("Buzz");
+            }
+            if (sb.length() == 0) {
+                sb.append(i);
+            }
+            list.add(sb.toString());
+        }
+        return list;
     }
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,10 +1,37 @@
 import org.junit.Test;
 
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
 public class FizzBuzzTest {
+
+    FizzBuzz fizzBuzz = new FizzBuzz();
 
     @Test
     public void test() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         fizzBuzz.fizzBuzz();
+    }
+
+    @Test
+    public void testN() {
+        List<String> expected = fizzBuzz.fizzBuzz(15);
+        assertEquals(15, expected.size());
+
+        assertEquals("1", expected.get(0));
+        assertEquals("2", expected.get(1));
+        assertEquals("Fizz", expected.get(2));
+        assertEquals("4", expected.get(3));
+        assertEquals("Buzz", expected.get(4));
+        assertEquals("Fizz", expected.get(5));
+        assertEquals("7", expected.get(6));
+        assertEquals("8", expected.get(7));
+        assertEquals("Fizz", expected.get(8));
+        assertEquals("Buzz", expected.get(9));
+        assertEquals("11", expected.get(10));
+        assertEquals("Fizz", expected.get(11));
+        assertEquals("13", expected.get(12));
+        assertEquals("14", expected.get(13));
+        assertEquals("FizzBuzz", expected.get(14));
     }
 }
